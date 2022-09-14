351617d03f889da5d57f55536190aa38d26de092
earth-bot-del <89304756+earth-bot-del@users.noreply.github.com>
2022-09-14 19:28:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function new_image(get_image)
             top:player_y,
             left:player_x
         });
-    canvas.add(black_image_object);
+    canvas.add(block_image_object);
     });
 }
 window.addEventListener("keydown", my_keydown);
@@ -116,5 +116,56 @@ function my_keydown(e)
     {
         new_image('cloud.jpg')
         console.log("c key is pressed");
+    }
+}
+function up()
+{
+    if(player_y>=0)
+    {
+        player_y=player_y-block_image_height;
+        console.log("block image height = "+ block_image_height);
+        console.log("when up arrow key is pressed, x = "+ player_x+" and y = "+ player_y);
+        canvas.remove(player_object);
+        player_update();
+
+    }
+}
+
+function down()
+{
+    if(player_y<=500)
+    {
+        player_y=player_y+block_image_height;
+        console.log("block image height = "+ block_image_height);
+        console.log("when down arrow key is pressed, x = "+ player_x+" and y = "+ player_y);
+        canvas.remove(player_object);
+        player_update();
+        
+    }
+}
+
+function left()
+{
+    if(player_x>=0)
+    {
+        player_x=player_x-block_image_width;
+        console.log("block image width = "+ block_image_width);
+        console.log("when left arrow key is pressed, x = "+ player_x+" and y = "+ player_y);
+        canvas.remove(player_object);
+        player_update();
+        
+    }
+}
+
+function right()
+{
+    if(player_x<=850)
+    {
+        player_x=player_x+block_image_width;
+        console.log("block image width = "+ block_image_width);
+        console.log("when right arrow key is pressed, x = "+ player_x+" and y = "+ player_y);
+        canvas.remove(player_object);
+        player_update();
+        
     }
 }
